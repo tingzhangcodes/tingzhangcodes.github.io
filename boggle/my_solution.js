6066b5e50783890d45ebbc301b35c2a3e2511c0e
Stephanie Zhang <stephtzhang@gmail.com>
2014-08-21 16:12:51 -0400

    update grid and possible words

--- boggle/my_solution.js
+++ boggle/my_solution.js
@@ -1,119 +1,3 @@
-// U3.W8-9: Challenge you're converting
-
-
-// I worked on this challenge by myself.
-
-// 2. Pseudocode
-// class BoggleBoard
-// takes two args board and possible_words
-// sets board to board
-// sets dictionary to possible_words
-// sets last_move to empty array
-// sets words to empty array 
-// sets coords to empty array 
-
-// function selectLetter
-// adds coordinates of a letter to coords array if it passes checkLetter
-
-// function checkLetter
-// checks whether the move is valid, including whether the coord exists on board, whether it has 
-// already been added to the coords array, and that it's touching the previously selected coord
-
-// function move
-// creates word from coords array by calling createWord
-// resets coords array and last_move array to empty arrays
-
-// function score
-// add word.length - 2 for all words in words array
-// return res 
-
-// function createWord
-// add each letter corresponding to coord in coords array to a string
-// return string
-
-
-// 3. Initial Solution
-// function BoggleBoard(board, possible_words) {
-// 	this.board = board;
-// 	this.dictionary = possible_words; 
-// 	this.last_move = [];
-// 	this.words = [];
-// 	this.coordinates = []; 
-// }
-
-// BoggleBoard.prototype.selectLetter = function(x,y) {
-// 	if(this.checkLetter(x,y)) {
-// 		this.coordinates.push([x,y]);
-// 		this.last_move = [x,y];
-// 	}
-// }
-
-// BoggleBoard.prototype.checkLetter = function(x,y) {
-// 	var check = false;
-// 	if(this.first_move()) {
-// 		check = true;
-// 	} else if (this.not_on_board(x,y) || this.already_picked(x,y) || this.not_touching(x,y)) {
-// 		check = false;
-// 		//DISPLAY SOME MESSAGE TO BROWSER WINDOW
-// 	} else {
-// 		check = true;
-// 	}
-// 	return check;
-// }
-
-// BoggleBoard.prototype.not_touching = function(x,y) {
-// 	return (x > this.last_move[0] + 1 || x < this.last_move[0] - 1 || y > this.last_move[1] + 1 || y < this.last_move[1] - 1);
-// }
-
-// BoggleBoard.prototype.first_move = function() {
-// 	return this.last_move.length == 0;
-// }
-
-
-// BoggleBoard.prototype.not_on_board = function(x,y) {
-// 	return this.board[x] == undefined || this.board[x][y] == undefined;
-// }
-
-// BoggleBoard.prototype.already_picked = function(x,y) {
-// 	var check = false;
-// 	for (i = 0; i < this.coordinates.length; i++) {
-// 		if(this.coordinates[i][0] == x && this.coordinates[i][1] == y) {
-// 			check = true;
-// 		}
-// 	}
-// 	return check;
-// }
-
-// BoggleBoard.prototype.createWord = function() {
-// 	var answer = "";
-// 	for (i = 0; i < this.coordinates.length; i++) {
-// 		var x = this.coordinates[i][0];
-// 		var y = this.coordinates[i][1];
-// 		answer = answer.concat(this.board[x][y]);
-// 	}
-// 	return answer;
-// }
-
-// BoggleBoard.prototype.move = function() {
-// 	this.words.push(this.createWord());
-// 	this.coordinates = [];
-// 	this.last_move = [];
-// }
-
-// BoggleBoard.prototype.score = function() {
-// 	var score = 0; 
-// 	for(i in this.words) {
-// 		for(j in this.dictionary) {
-// 			if(this.words[i] == this.dictionary[j]) {
-// 				score = score + this.words[i].length - 2;
-// 			}
-// 		}
-// 	}
-// 	return score; 
-// }
-
-
-// 4. Refactored Solution
 function BoggleBoard(board, possible_words) {
 	this.board = board;
 	this.dictionary = possible_words; 
@@ -223,16 +107,12 @@ BoggleBoard.prototype.displayLetters = function() {
 }
 
 
-
-
-
-// 1. DRIVER TESTS/ASSERT STATEMENTS GO BELOW THIS LINE
-grid = [["b", "r", "a", "e"],
-       ["i", "o", "d", "t"],
-       ["e", "c", "l", "r"],
-       ["t", "a", "k", "e"]];
-
-possible_words = ["code","locate","trek","take","cake","boca","date","lake"];
+grid = [["a", "i", "i", "o"],
+       ["d", "y", "e", "g"],
+       ["n", "r", "t", "t"],
+       ["a", "n", "e", "t"]];
+// possible_words from http://www.wordplays.com/boggle
+possible_words = ["dieter", "entera", "gerent", "getter", "nereid", "netter", "tenter", "tetter", "antre", "arete", "enter", "entry", "nerdy", "netty","randy","rente","teiid","tenty","tenty","terne","tetra","ante","arty","diet","dreg","dyer","erne","etna","eyra","eyre","goer","nard","nary","nerd","nett","rand","rant","rent","rete","rynd","tent","tern","tret","trey","tyer","tyre","yett","aid","and","ane","ant","any","are","art","aye","day","die","dry","dye","ego","era","ere","ern","get","gey","gie","nan","net","ran","reg","rei","ret","rya","rye","teg","ten","tet","try","tye","yet","yid"];
 
 //modified from http://stackoverflow.com/questions/15164655/generate-html-table-from-2d-javascript-array
 function createTable(tableData) {
@@ -253,38 +133,8 @@ function createTable(tableData) {
 
   table.appendChild(tableBody);
 
-  // not working:
-  // var container = document.getElementById("board-container");
-  // container.parentNode.insertBefore(table, container);
-  // container.appendChild(table);
   document.body.appendChild(table);
 }
 
 board = new BoggleBoard(grid, possible_words);
 createTable(grid)
-
-
-
-// TERMINAL DRIVER TESTS
-// board = new BoggleBoard(grid, possible_words);
-// board.selectLetter(0,0);
-// board.selectLetter(1,1);
-// board.selectLetter(3,3); // => won't be added to word bc not_touching
-// console.log(board.not_touching(3,3)==true);
-// board.selectLetter(4,4); // => won't be added to word bc not_on_board
-// console.log(board.not_on_board(4,4)==true);
-// console.log(board.not_on_board(-1,-1)==true);
-// board.selectLetter(2,1);
-// board.selectLetter(2,1); // => won't be added to word bc already_picked
-// console.log(board.already_picked(2,1)==true);
-// board.selectLetter(3,1);
-// console.log(board.createWord()=="boca");
-// board.move();
-// console.log(board.score()==2);
-
-
-
-
-
-
-// 5. Reflection 
