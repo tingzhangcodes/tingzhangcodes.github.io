f754d6d0b89a70aeaff47c55bd05e57f62667a29
stephtzhang <stephtzhang@gmail.com>
2014-12-10 11:48:29 -0500

    Semantically rename game file

--- boggle/game.js
+++ boggle/game.js
@@ -106,35 +106,3 @@ BoggleBoard.prototype.displayLetters = function() {
 	document.getElementById("letters").innerHTML = "Current letters: "+this.createWord();
 }
 
-
-grid = [["a", "i", "i", "o"],
-       ["d", "y", "e", "g"],
-       ["n", "r", "t", "t"],
-       ["a", "n", "e", "t"]];
-// possible_words from http://www.wordplays.com/boggle
-possible_words = ["dieter", "entera", "gerent", "getter", "nereid", "netter", "tenter", "tetter", "antre", "arete", "enter", "entry", "nerdy", "netty","randy","rente","teiid","tenty","tenty","terne","tetra","ante","arty","diet","dreg","dyer","erne","etna","eyra","eyre","goer","nard","nary","nerd","nett","rand","rant","rent","rete","rynd","tent","tern","tret","trey","tyer","tyre","yett","aid","and","ane","ant","any","are","art","aye","day","die","dry","dye","ego","era","ere","ern","get","gey","gie","nan","net","ran","reg","rei","ret","rya","rye","teg","ten","tet","try","tye","yet","yid"];
-
-//modified from http://stackoverflow.com/questions/15164655/generate-html-table-from-2d-javascript-array
-function createTable(tableData) {
-  var table = document.createElement('table')
-    , tableBody = document.createElement('tbody');
-
-  tableData.forEach(function(rowData) {
-    var row = document.createElement('tr');
-
-    rowData.forEach(function(cellData) {
-      var cell = document.createElement('td');
-      cell.appendChild(document.createTextNode(cellData));
-      row.appendChild(cell);
-    });
-
-    tableBody.appendChild(row);
-  });
-
-  table.appendChild(tableBody);
-
-  document.body.appendChild(table);
-}
-
-board = new BoggleBoard(grid, possible_words);
-createTable(grid)
