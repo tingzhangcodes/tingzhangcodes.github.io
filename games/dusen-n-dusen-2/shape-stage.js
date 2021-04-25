3454f4bfee0e537e8bf8721a361919058ef7f361
ting zhang <thetingzhang@gmail.com>
2021-04-25 11:10:40 -0400

    [Add] ability to hide stages

--- games/dusen-n-dusen-2/shape-stage.js
+++ games/dusen-n-dusen-2/shape-stage.js
@@ -6,9 +6,17 @@ const COLORS = [ORANGE, BLUE, GREEN, MAGENTA];
 
 
 class ShapeStage {
-  constructor(container, stageId) {
+  constructor(stageId, visible=true) {
+    var container = document.getElementById("container");
+    var stageDiv = document.createElement("div");
+    stageDiv.id = stageId;
+    if (visible !== true) {
+      stageDiv.style.display = "none";
+    }
+    container.appendChild(stageDiv)
+
     this.stage = new Konva.Stage({
-      container: container,
+      container: stageId,
       width: window.innerWidth,
       height: window.innerHeight,
     });
@@ -40,6 +48,19 @@ class ShapeStage {
     // https://fontawesome.com/icons/arrow-alt-circle-right
     const iconFile = 'arrow-alt-circle-right-solid.svg';
     this.showIcon(iconFile, toggleInfo, 'right')
+  }
+
+
+  showPrevIcon() {
+    // https://fontawesome.com/icons/arrow-alt-circle-left
+    const iconFile = 'arrow-alt-circle-left-solid.svg';
+    this.showIcon(iconFile, toggleInfo, 'left')
+  }
+
+  showDownloadIcon() {
+    // https://fontawesome.com/icons/arrow-alt-circle-down
+    const iconFile = 'arrow-alt-circle-down-solid.svg';
+    this.showIcon(iconFile, downloadShapes, 'right')
   }
 
 
@@ -185,4 +206,58 @@ function makeShape(x, y, stageWidth) {
   addCursorStyling(shape);
 
   return shape;
+}
+
+
+// function from https://stackoverflow.com/a/15832662/512042
+function downloadURI(uri, name) {
+  var link = document.createElement('a');
+  link.download = name;
+  link.href = uri;
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+  delete link;
+}
+
+
+// https://stackoverflow.com/questions/3318565/any-way-to-clone-html5-canvas-element-with-its-content
+function cloneCanvas(oldCanvas) {
+  //create a new canvas
+  var newCanvas = document.createElement('canvas');
+  var context = newCanvas.getContext('2d');
+
+  //set dimensions
+  newCanvas.width = oldCanvas.width;
+  newCanvas.height = oldCanvas.height;
+
+  //apply the old canvas to the new one
+  context.drawImage(oldCanvas, 0, 0);
+
+  //return the new canvas
+  return newCanvas;
+}
+
+
+function downloadShapes() {
+  var canvasList = document.getElementsByTagName("canvas");
+  var leftCanvas = canvasList[0];
+  var leftCtx = leftCanvas.getContext("2d");
+  var rightCanvas = canvasList[1];
+  var rightCtx = rightCanvas.getContext("2d");
+  var ctxList = [leftCtx, rightCtx];
+  var tempCanvas = cloneCanvas(leftCanvas);
+  var tempCtx = tempCanvas.getContext("2d")
+
+  const step = 25;
+  for (let pixel = 0; pixel < tempCanvas.width; pixel += step) {
+    // i will toggle from 0 to 1 and back with each iteration
+    var i = (pixel / step) % 2;
+    // https://developer.mozilla.org/en-US/docs/Web/API/Canvas_API/Tutorial/Pixel_manipulation_with_canvas
+    var imageData = ctxList[i].getImageData(pixel, 0, step, tempCanvas.height);
+    tempCtx.putImageData(imageData, pixel, 0);
+  }
+
+  var dataURL = tempCanvas.toDataURL("image/png");
+  downloadURI(dataURL, 'dusen-n-dusen.png');
 }
