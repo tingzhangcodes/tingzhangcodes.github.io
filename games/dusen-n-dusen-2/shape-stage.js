87861ef23202a62f108d5f5c4238d88f5add71c6
ting zhang <thetingzhang@gmail.com>
2021-04-25 11:55:56 -0400

    [Remove] unused func

--- games/dusen-n-dusen-2/shape-stage.js
+++ games/dusen-n-dusen-2/shape-stage.js
@@ -265,24 +265,6 @@ function downloadURI(uri, name) {
 }
 
 
-// https://stackoverflow.com/questions/3318565/any-way-to-clone-html5-canvas-element-with-its-content
-function cloneCanvas(oldCanvas) {
-  //create a new canvas
-  var newCanvas = document.createElement('canvas');
-  var context = newCanvas.getContext('2d');
-
-  //set dimensions
-  newCanvas.width = oldCanvas.width;
-  newCanvas.height = oldCanvas.height;
-
-  //apply the old canvas to the new one
-  context.drawImage(oldCanvas, 0, 0);
-
-  //return the new canvas
-  return newCanvas;
-}
-
-
 function getStageCanvas(stageId) {
   var stageDiv = document.getElementById(stageId);
   var stageCanvas = stageDiv.getElementsByTagName('canvas')[0];
