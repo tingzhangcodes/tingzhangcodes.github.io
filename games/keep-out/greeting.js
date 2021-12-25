22d5a98fc7c752377e822478971a39850e863f90
ting <thetingzhang@gmail.com>
2021-12-25 10:47:50 -0500

    [Add] secret code fetch (#24)

--- games/keep-out/greeting.js
+++ games/keep-out/greeting.js
@@ -14,9 +14,20 @@ there are 4 hidden elements that can be unlocked with the right codes. \n\
 when you find all 4, you'll unlock the key to a prize. \n\
 good luck!", "color: green")
 
-var displayPrize = function() {
+var getCode = async function() {
+  const url = "https://gist.githubusercontent.com/tngzng/c81813a9b7c30f3ec546fdb68a188c7d/raw/f6d846baae65ed6d94dd64e43592a1574d56d8f9/keep-out-code.txt"
+  try {
+    const fetchResponse = await fetch(url);
+    const resText = await fetchResponse.text();
+    return resText;
+  } catch (err) {
+    console.error('Error - ', err);
+  }
+}
+
+var displayPrize = async function() {
     console.log(
-         "%ccongratulations!!! you've unlocked the key to the prize:",
+         "%ccongratulations! you've unlocked the key to the prize!!!",
          "color:yellow"
     )
     console.log(`%c
@@ -26,7 +37,10 @@ var displayPrize = function() {
        '--'
     `, "font-family:monospace; color:yellow")
     console.log(
-         "%cto claim your prize text or email ting with the following code:",
+         "%cto claim your prize email ting the following code:",
          "color:yellow"
     )
+    var code = await getCode();
+    console.log(`%c${code}`,
+                "font-family:monospace; color:yellow")
 }
